7397040463f8f873ce57fd04749580176256ccac
madison <madisoncraig@protonmail.com>
2023-10-02 13:18:32 -0700

    more stuff

--- bevip.sh
+++ bevip.sh
@@ -51,6 +51,12 @@ function sys_enum()
     printf "\n[+]User ID:" | tee -a $logfile
     id | tee -a $logfile 1>&2
     printf "\n" | tee -a $logfile
+
+    sudoperms=$(echo '' | sudo -S -l -k 2>/dev/null)
+    if [ "$sudoperms" ]; then
+        printf "\n[+]Passwordless Sudo Permissions: $sudoperms\n" | tee -a $logfile
+    fi
+    printf "\n" | tee -a $logfile
     
     printf "[+]Other Users:\n" | tee -a $logfile
     cat /etc/passwd | tee -a $logfile 1>&2
@@ -100,53 +106,80 @@ function sys_enum()
     printf "\n[+]System Logs:\n" | tee -a $logfile
     find / -name "*.log" | tee -a $logfile 1>&2
 
+    
+    all_binaries
+
     printf "\n[!]Finished!\n\n" | tee -a $logfile
     sleep 1
     main
 }
 
-function applications()
+function common_binaries()
+{   
+    printf "\n[${G}+${N}]Commonly Installed Binaries: \n" | tee -a $logfile
+    which curl 2>/dev/null | tee -a $logfile
+    which gcc 2>/dev/null | tee -a $logfile
+    which mknod 2>/dev/null | tee -a $logfile
+    which netcat 2>/dev/null | tee -a $logfile
+    which nmap 2>/dev/null | tee -a $logfile
+    which perl 2>/dev/null | tee -a $logfile
+    which python 2>/dev/null | tee -a $logfile
+    which ssh 2>/dev/null | tee -a $logfile
+    which tcpdump 2>/dev/null | tee -a $logfile
+    which telnet 2>/dev/null | tee -a $logfile
+    which wget 2>/dev/null | tee -a $logfile
+}
+
+function all_binaries()
+{
+    printf "\n[${G}+${N}]All Insatlled Binaries: \n" | tee -a $logfile
+    ls -alh /usr/bin | tee -a $logfile 1>&2
+    ls -alh /usr/local/bin | tee -a $logfile 1>&2
+    ls -alh /opt/ | tee -a $logfile 1>&2
+    sleep 0.5 && clear 2>/dev/null
+}
+
+function binaries()
 {       banner
-    printf "\n[${G}+${N}]Commonly Installed Applications:\n"
-    which curl 2>/dev/null
-    which gcc 2>/dev/null
-    which mknod 2>/dev/null
-    which netcat 2>/dev/null
-    which nmap 2>/dev/null
-    which perl 2>/dev/null
-    which python 2>/dev/null
-    which ssh 2>/dev/null
-    which tcpdump 2>/dev/null
-    which telnet 2>/dev/null
-    which wget 2>/dev/null
-    
-    printf "\n[${R}!${N}]Finished! Would you like to save all installed applications to a log file? \n"
-    read -p '[?]Proceed? Y/n: ' answer
-    if [[ $answer == 'y' ]]; then
+        common_binaries
+        printf "\n[${R}!${N}]Would you like to save installed binaries to a log file? \n"
+    read -p '[?]Proceed? Y/n/A(all binaries): ' saveanswer
+    if [[ $saveanswer == 'y' ]]; then
         output
-        printf "\n[+]Installed Applications:\n"
-        ls -alh /usr/bin | tee -a $logfile 1>&2
-        ls -alh /usr/local/bin | tee -a $logfile 1>&2
-        ls -alh /opt/ | tee -a $logfile 1>&2
-        printf "\n"
-    else
-        echo ""
+        common_binaries
+        elif [[ $saveanswer == 'a' ]]; then
+            output
+            all_binaries
+        else
+            echo ""
     fi
     main
 }
 
 function user_passwords()
 {       banner
-    printf "\n[${G}?${N}]Would you like to look for username & passwords in clear text files?\n[${G}+${N}]Currently .php .sql .txt files\n\n"
+    printf "\n[${G}?${N}]Would you like to look for username & passwords in clear text files and save them to an output file?\n[${G}+${N}]Currently will search .php .sql & .txt files\n\n"
     read -p '[?]Proceed? Y/n: ' answer
     if [[ $answer == 'n' ]]; then
         main
     else
         output
 
-        printf "\nPHP Files\n" | tee -a $logfile
-
-
+        printf "\n[+]PHP Files\n" | tee -a $logfile
+        find / -name "*.php" -print0 | xargs -0 grep -i -n "var password" | tee -a $logfile 1>&2
+        find / -name "*.php" -print0 | xargs -0 grep -i -n "var user" | tee -a $logfile 1>&2
+        sleep 0.5 && clear 2>/dev/null
+        
+        printf "\n[+]SQL Files\n" | tee -a $logfile
+        find / -name "*.sql" -print0 | xargs -0 grep -i -n "password" | tee -a $logfile 1>&2
+        find / -name "*.sql" -print0 | xargs -0 grep -i -n "user" | tee -a $logfile 1>&2
+        sleep 0.5 && clear 2>/dev/null
+        
+        printf "\n[+]SQL Files\n" | tee -a $logfile
+        find / -name "*.txt" -print0 | xargs -0 grep -i -n "password" | tee -a $logfile 1>&2
+        find / -name "*.txt" -print0 | xargs -0 grep -i -n "username" | tee -a $logfile 1>&2
+        sleep 0.5 && clear 2>/dev/null
+        main
     fi
     main
 }
@@ -154,15 +187,15 @@ function user_passwords()
 function main()
 {       banner
     PS3='Choose an option: '
-    options=("System Enumeration" "Installed Applications" "User:Passwords" "Quit")
+    options=("System Enumeration" "Installed Binaries" "User:Passwords" "Quit")
     select opt in "${options[@]}"
     do
         case $opt in
             "System Enumeration")
                 sys_enum
                 ;;
-            "Installed Applications")
-                applications
+            "Installed Binaries")
+                binaries
                 ;;
             "User:Passwords")
                 user_passwords
